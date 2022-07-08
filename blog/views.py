ccf9fe25a620a5fd504a0f3dbbb0949a20d1e7d1
Mgbemene Comfort <mgbemenecomfortchika@gmail.com>
2022-07-08 04:30:15 +0100

    modified views

--- blog/views.py
+++ blog/views.py
@@ -1,37 +1,26 @@
-from ast import Return
-from dataclasses import fields
-from multiprocessing import context
-from pydoc import render_doc
-from pyexpat import model
-from sre_constants import SUCCESS
 from django.shortcuts import render
-from django.http import HttpResponse
+from django.views import generic
 from django.urls import reverse_lazy
-# Create your views here.
 from .models import Post
 
-def PostListView(Post):
-    context = {}
-    model = Post
-    Return render(Post, 'Blog/post_list.html', context)
+# Create your views here.
+class PostListView(generic.ListView):
+  model = Post
 
-def PostCreateView(Post):
-    context = {}
-    model = Post
-    fields = "__all__"
-    SUCCESS_url = reverse_lazy("blog:all")
-    Return render(Post, 'Blog/post_list.html', context)
+class PostCreateView(generic.CreateView):
+  model = Post
+  fields = "__all__"
+  success_url = reverse_lazy("blog:all")
 
-def PostDetailsView(Post):
-    context = {}
-    model = Post
+class PostDetailView(generic.DetailView):
+  model = Post
 
-def PostUpdateView(Post):
-    model = Post
-    fields = "__all__"
-    SUCCESS_url = reverse_lazy("blog:all")
+class PostUpdateView(generic.UpdateView):
+  model = Post
+  fields = "__all__"
+  success_url = reverse_lazy("blog:all")
 
-def PostDeleteView(Post):
-    model = Post
-    fields = "__all__"
-    SUCCESS_url = reverse_lazy("blog:all")
+class PostDeleteView(generic.UpdateView):
+  model = Post
+  fields = "__all__"
+  success_url = reverse_lazy("blog:all")
